5ca48982106ecd95f3fa47f12156e44d5152ea96
Aarishmughal <aarishmughal21@gmail.com>
2025-01-27 01:54:16 +0500

    Somewhat Complete

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -16,7 +16,8 @@
             padding: 0;
             width: 100vw;
             height: 100vh;
-            overflow: hidden;
+            overflow-x: hidden;
+            overflow-y: auto;
         }
 
         #back-button {
@@ -39,127 +40,152 @@
     </a>
 
     <!-- Main Content -->
-    <div class="row m-0">
-        <div class="container d-flex justify-content-center align-items-center vh-100">
-            <div class="col-lg-6 col-sm-10">
-                <div class="card shadow-lg p-3">
-                    <div class="card-body">
-                        <h3 class="card-title mb-4">Register Administrator</h3>
-                        <form action="{{ route('register') }}" method="POST">
-                            @csrf
-                            <div class="row">
-                                <div class="col-lg">
-                                    <div class="form-floating mb-3">
-                                        <input type="text" class="form-control @error('fname') is-invalid @enderror"
-                                            value="{{ old('fname') }}" id="fname" name="fname"
-                                            placeholder="First Name">
-                                        <label for="fname">First Name<span class="text-danger">*</span></label>
-                                        @if ($errors->has('fname'))
-                                        <span
-                                            class="text-danger small custom-error m-0">{{ $errors->first('fname') }}</span>
-                                        @endif
-                                    </div>
-                                </div>
-                                <div class="col-lg">
-                                    <div class="form-floating mb-3">
-                                        <input type="text" class="form-control @error('lname') is-invalid @enderror" id="lname" name="lname"
-                                            placeholder="Last Name">
-                                        <label for="lname">Last Name<span class="text-danger">*</span></label>
-                                        @if ($errors->has('lname'))
-                                        <span
-                                            class="text-danger small custom-error m-0">{{ $errors->first('lname') }}</span>
-                                        @endif
-                                    </div>
+    <div class="row m-0 d-flex justify-content-center align-items-center vh-100">
+        <div class="col-lg-6 col-sm-10">
+            <div class="card shadow-lg p-3">
+                <div class="card-body">
+                    <h3 class="card-title mb-4">Register Administrator</h3>
+                    <form action="{{ route('register') }}" method="POST">
+                        @csrf
+                        <div class="row">
+                            <div class="col-lg">
+                                <div class="form-floating mb-3">
+                                    <input type="text" class="form-control @error('fname') is-invalid @enderror"
+                                        value="{{ old('fname') }}" id="fname" name="fname"
+                                        placeholder="First Name">
+                                    <label for="fname">First Name<span class="text-danger">*</span></label>
+                                    @if ($errors->has('fname'))
+                                    <span
+                                        class="text-danger small custom-error m-0">{{ $errors->first('fname') }}</span>
+                                    @endif
                                 </div>
                             </div>
-                            <div class="row">
-                                <div class="col-lg">
-                                    <div class="form-floating mb-3">
-                                        <input type="text" class="form-control @error('username') is-invalid @enderror" id="username" name="username"
-                                            placeholder="Username">
-                                        <label for="username">Username<span class="text-danger">*</span></label>
-                                        @if ($errors->has('username'))
-                                        <span
-                                            class="text-danger small custom-error m-0">{{ $errors->first('username') }}</span>
-                                        @endif
-                                    </div>
-                                </div>
-                                <div class="col-lg">
-                                    <div class="form-floating mb-3">
-                                        <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" name="email"
-                                            placeholder="name@example.com">
-                                        <label for="email">Email address<span class="text-danger">*</span></label>
-                                        @if ($errors->has('email'))
-                                        <span
-                                            class="text-danger small custom-error m-0">{{ $errors->first('email') }}</span>
-                                        @endif
-                                    </div>
+                            <div class="col-lg">
+                                <div class="form-floating mb-3">
+                                    <input type="text" class="form-control @error('mname') is-invalid @enderror" id="mname" name="mname"
+                                        placeholder="Middle Name">
+                                    <label for="mname">Middle Name<span class="text-danger">*</span></label>
+                                    @if ($errors->has('mname'))
+                                    <span
+                                        class="text-danger small custom-error m-0">{{ $errors->first('mname') }}</span>
+                                    @endif
                                 </div>
                             </div>
-                            <div class="row">
-                                <div class="col-lg">
-                                    <div class="form-floating mb-3">
-                                        <select class="form-select @error('gender') is-invalid @enderror" id="gender" name="gender">
-                                            <option disabled selected>Select an Option</option>
-                                            <option value="male">Male</option>
-                                            <option value="female">Female</option>
-                                        </select>
-                                        <label for="gender">Gender</label>
-                                        @if ($errors->has('gender'))
-                                        <span
-                                            class="text-danger small custom-error m-0">{{ $errors->first('gender') }}</span>
-                                        @endif
-                                    </div>
-                                </div>
-                                <div class="col-lg">
-                                    <div class="input-group mb-3">
-                                        <span class="input-group-text" style="font-family: 'Inter">+92</span>
-                                        <div class="form-floating flex-grow-1">
-                                            <input type="number" class="form-control @error('phone') is-invalid @enderror" name="phone" id="gender"
-                                                placeholder="Phone Number">
-                                            <label for="phone">Phone Number</label>
-                                            @if ($errors->has('phone'))
-                                            <span
-                                                class="text-danger small custom-error m-0">{{ $errors->first('phone') }}</span>
-                                            @endif
-                                        </div>
-                                    </div>
+                            <div class="col-lg">
+                                <div class="form-floating mb-3">
+                                    <input type="text" class="form-control @error('lname') is-invalid @enderror" id="lname" name="lname"
+                                        placeholder="Last Name">
+                                    <label for="lname">Last Name<span class="text-danger">*</span></label>
+                                    @if ($errors->has('lname'))
+                                    <span
+                                        class="text-danger small custom-error m-0">{{ $errors->first('lname') }}</span>
+                                    @endif
                                 </div>
                             </div>
-                            <div class="row">
-                                <div class="col-lg">
-                                    <div class="form-floating mb-3">
-                                        <input type="password" class="form-control @error('password') is-invalid @enderror" id="password" name="password"
-                                            placeholder="Password">
-                                        <label for="password">Password<span class="text-danger">*</span></label>
-                                        @if ($errors->has('password'))
-                                        <span
-                                            class="text-danger small custom-error m-0">{{ $errors->first('password') }}</span>
-                                        @endif
-                                    </div>
-                                </div>
-                                <div class="col-lg">
-                                    <div class="form-floating mb-3">
-                                        <input type="password" class="form-control @error('password') is-invalid @enderror" id="password_confirmation"
-                                            name="password_confirmation" placeholder="Confirm Password">
-                                        <label for="password_confirmation">Confirm Password<span
-                                                class="text-danger">*</span></label>
-                                        @if ($errors->has('password'))
-                                        <span
-                                            class="text-danger small custom-error m-0">{{ $errors->first('password') }}</span>
-                                        @endif
-                                    </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg">
+                                <div class="form-floating mb-3">
+                                    <input type="text" class="form-control @error('username') is-invalid @enderror" id="username" name="username"
+                                        placeholder="Username">
+                                    <label for="username">Username<span class="text-danger">*</span></label>
+                                    @if ($errors->has('username'))
+                                    <span
+                                        class="text-danger small custom-error m-0">{{ $errors->first('username') }}</span>
+                                    @endif
                                 </div>
                             </div>
-                            <button type="submit" class="btn btn-light w-100">Register</button>
-                        </form>
-                    </div>
+                            <div class="col-lg">
+                                <div class="form-floating mb-3">
+                                    <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" name="email"
+                                        placeholder="name@example.com">
+                                    <label for="email">Email address<span class="text-danger">*</span></label>
+                                    @if ($errors->has('email'))
+                                    <span
+                                        class="text-danger small custom-error m-0">{{ $errors->first('email') }}</span>
+                                    @endif
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg">
+                                <div class="form-floating mb-3">
+                                    <select class="form-select @error('gender') is-invalid @enderror" id="gender" name="gender">
+                                        <option disabled selected>Select an Option</option>
+                                        <option value="male">Male</option>
+                                        <option value="female">Female</option>
+                                    </select>
+                                    <label for="gender">Gender</label>
+                                    @if ($errors->has('gender'))
+                                    <span
+                                        class="text-danger small custom-error m-0">{{ $errors->first('gender') }}</span>
+                                    @endif
+                                </div>
+                            </div>
+                            <div class="col-lg">
+                                <div class="input-group mb-3">
+                                    <span class="input-group-text" style="font-family: 'Inter">+92</span>
+                                    <div class="form-floating flex-grow-1">
+                                        <input type="number" class="form-control @error('phone') is-invalid @enderror" name="phone" id="gender"
+                                            placeholder="Phone Number">
+                                        <label for="phone">Phone Number</label>
+                                    </div>
+                                </div>
+                                @if ($errors->has('phone'))
+                                <div>
+                                    <span class="text-danger small custom-error m-0">{{ $errors->first('phone') }}</span>
+                                </div>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg">
+                                <div class="form-floating mb-3">
+                                    <input type="password" class="form-control @error('password') is-invalid @enderror" id="password" name="password"
+                                        placeholder="Password">
+                                    <label for="password">Password<span class="text-danger">*</span></label>
+                                    @if ($errors->has('password'))
+                                    <span
+                                        class="text-danger small custom-error m-0">{{ $errors->first('password') }}</span>
+                                    @endif
+                                </div>
+                            </div>
+                            <div class="col-lg">
+                                <div class="form-floating mb-3">
+                                    <input type="password" class="form-control @error('password') is-invalid @enderror" id="password_confirmation"
+                                        name="password_confirmation" placeholder="Confirm Password">
+                                    <label for="password_confirmation">Confirm Password<span
+                                            class="text-danger">*</span></label>
+                                    @if ($errors->has('password'))
+                                    <span
+                                        class="text-danger small custom-error m-0">{{ $errors->first('password') }}</span>
+                                    @endif
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col-lg">
+                                <div class="form-floating">
+                                    <textarea class="form-control" placeholder="Bio" id="floatingTextarea2" style="height: 100px"></textarea>
+                                    <label for="floatingTextarea2">Short Bio</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col-lg">
+                                <p class="mb-1">Social Media Links</p>
+                                <button class="btn btn-light" onclick="addSocialMediaRow(event)">Add Link</button>
+                            </div>
+                        </div>
+                        <div id="socialMediaLinks" class="mb-3"></div>
+                        <button type="submit" class="btn btn-light w-100">Register</button>
+                    </form>
                 </div>
-
-                <p class="text-body-secondary mt-3">
-                    &copy; {{ __(now()->year) }} e-lite, Inc
-                </p>
             </div>
+
+            <p class="text-body-secondary mt-3">
+                &copy; {{ __(now()->year) }} e-lite, Inc
+            </p>
         </div>
     </div>
 
